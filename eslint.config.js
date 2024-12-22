775c1b94faa2e31487fd1ad3ba2ab11c2c330f13
WilliamAkingba <x00179468@mytudublin.ie>
2024-12-23 04:34:47 +0500

    Code correction

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
-// eslint.config.js
 module.exports = [
     {
-        files: ['**/*.js'], // Apply to all .js files
+        files: ['**/*.js'], 
         languageOptions: {
-            ecmaVersion: 2021, // Use ECMAScript 2021
-            sourceType: 'module', // Allow ES modules
+            ecmaVersion: 2021, 
+            sourceType: 'module', 
             globals: {
-                // Node.js globals
                 module: 'readonly',
                 require: 'readonly',
-                // Jest globals
                 describe: 'readonly',
                 console: 'readonly',
                 test: 'readonly',
@@ -20,20 +17,16 @@ module.exports = [
             },
         },
         plugins: {
-            // Include security plugin
             security: require('eslint-plugin-security'),
         },
         rules: {
-            // Existing rules
-            'no-unused-vars': 'warn', // Warn on unused variables
-            'no-undef': 'error', // Error on undefined variables
-            
-            // Security-specific rules
-            'security/detect-object-injection': 'warn', // Warn on unsafe object injection
-            'security/detect-unsafe-regex': 'error', // Error on unsafe regular expressions
-            'security/detect-buffer-noassert': 'warn', // Warn on insecure Buffer methods
-            'security/detect-child-process': 'error', // Error on use of child_process
-            'security/detect-eval-with-expression': 'error', // Error on use of eval()
+            'no-unused-vars': 'warn',
+            'no-undef': 'error',
+            'security/detect-object-injection': 'warn',
+            'security/detect-unsafe-regex': 'error',
+            'security/detect-buffer-noassert': 'warn',
+            'security/detect-child-process': 'error',
+            'security/detect-eval-with-expression': 'error',
         },
     },
 ];
