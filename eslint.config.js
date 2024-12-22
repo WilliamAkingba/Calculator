de4ed23e062d2834050f0d970318f2bb9b1abfec
WilliamAkingba <x00179468@mytudublin.ie>
2024-12-23 01:58:05 +0500

    Update ESLint config to allow console

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ module.exports = [
                 require: 'readonly',
                 // Jest globals
                 describe: 'readonly',
+                console: 'readonly',
                 test: 'readonly',
                 expect: 'readonly',
                 beforeEach: 'readonly',
