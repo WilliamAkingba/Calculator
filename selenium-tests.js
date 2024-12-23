600e3aea72a403467d79926943fb65037fd660db
WilliamAkingba <x00179468@mytudublin.ie>
2024-12-23 14:21:58 +0500

    Demo

--- selenium-tests.js
+++ selenium-tests.js
@@ -3,7 +3,6 @@ const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 
 (async function example() {
-    // Configure ChromeDriver to run in headless mode
     const options = new chrome.Options();
     options.addArguments('--headless', '--no-sandbox', '--disable-dev-shm-usage');
 
@@ -13,23 +12,19 @@ const path = require('path');
         .build();
 
     try {
-        // Load the HTML file directly from the file system
         const filePath = `file://${path.join(process.cwd(), 'test-page.html')}`;
         console.log(`Loading file from: ${filePath}`);
         await driver.get(filePath);
 
-        // Test Addition
         await driver.findElement(By.id('num1')).sendKeys('5');
         await driver.findElement(By.id('num2')).sendKeys('3');
         await driver.findElement(By.id('add-btn')).click();
         const addResult = await driver.findElement(By.id('result')).getText();
         console.log(`Addition Test Result: ${addResult}`);
 
-        // Clear inputs for the next test
         await driver.findElement(By.id('num1')).clear();
         await driver.findElement(By.id('num2')).clear();
 
-        // Test Subtraction
         await driver.findElement(By.id('num1')).sendKeys('8');
         await driver.findElement(By.id('num2')).sendKeys('2');
         await driver.findElement(By.id('sub-btn')).click();
